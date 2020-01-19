d3bc7f75fd65d46c705fc436801bc33c50c60c51
Mark <mark@remarkablemark.org>
2020-01-19 16:31:41 -0500

    feat(src): add `index.ts` with phonetic alphabet map and converter
    Add NATO phonetic alphabet (lowercased):
    https://en.wikipedia.org/wiki/NATO_phonetic_alphabet
    
    Add and export converter function

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+type PhoneticAlphabet = Record<string, string>;
+
+const nato: PhoneticAlphabet = {
+  a: 'alpha',
+  b: 'bravo',
+  c: 'charlie',
+  d: 'delta',
+  e: 'echo',
+  f: 'foxtrot',
+  g: 'golf',
+  h: 'hotel',
+  i: 'indiana',
+  j: 'juliet',
+  k: 'kilo',
+  l: 'lima',
+  m: 'mike',
+  n: 'november',
+  o: 'oscar',
+  p: 'papa',
+  q: 'quebec',
+  r: 'romeo',
+  s: 'sierra',
+  t: 'tango',
+  u: 'uniform',
+  v: 'victor',
+  w: 'whiskey',
+  x: 'x-ray',
+  y: 'yankee',
+  z: 'zulu'
+};
+
+/**
+ * Converts a string to an array of (NATO) phonetic alphabet words.
+ */
+function converter(text: string, alphabet: PhoneticAlphabet = nato): string[] {
+  return text
+    .toLowerCase()
+    .split('')
+    .map(letter => alphabet[letter]);
+}
+
+export default converter;
