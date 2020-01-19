0ae9d4a09ce32fa21f958362ada33b2810b42714
Mark <mark@remarkablemark.org>
2020-01-19 18:45:38 -0500

    build(package): build umd bundle with rollup
    We are doing this because TypeScript does not support umd that
    sets a global variable/namespace.
    
    - Save to "devDependencies" in package.json
      - @rollup/plugin-typescript@2.1.0
      - rollup@1.29.0
    - Create and update npm scripts `build:umd` and `build`
    - Add `umd/` to package.json "files" and .gitignore
    - Init `rollup.config.js`

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'umd/phonetic-alphabet-converter.js',
+    format: 'umd',
+    name: 'PhoneticAlphabetConverter'
+  },
+  plugins: [typescript()]
+};
