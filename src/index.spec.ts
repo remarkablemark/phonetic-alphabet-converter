54cf94b684d5789e9a1d42a64ba75a8c65ad5dd9
Mark <mark@remarkablemark.org>
2020-01-28 00:04:59 -0500

    test(src): check that converter throws error and converts correctly

---
+++ src/index.spec.ts
@@ -0,0 +1,28 @@
+import converter from '.';
+
+describe('error', () => {
+  it.each([
+    undefined,
+    null,
+    0,
+    1,
+    {},
+    [],
+    () => undefined,
+    new Date(),
+    Symbol()
+  ])('throws when first argument is %s', value => {
+    expect(() => {
+      converter(value as string);
+    }).toThrowError('First argument must be a string');
+  });
+});
+
+describe('converter', () => {
+  it.each([
+    ['mark', ['mike', 'alpha', 'romeo', 'kilo']],
+    ['abc', ['alpha', 'bravo', 'charlie']]
+  ])('converts "%s" correctly', (text, expected) => {
+    expect(converter(text)).toEqual(expected);
+  });
+});
