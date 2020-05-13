796722a0a293a90e46c8b7bd9240b5c0f18ce2ba
Mark <mark@remarkablemark.org>
2020-05-12 22:37:11 -0400

    refactor(docs): move custom script from index.html to script.js

---
+++ docs/script.js
@@ -0,0 +1,48 @@
+/**
+ * Load.
+ */
+var script = document.createElement('script');
+script.src =
+  location.hostname === ''
+    ? '../umd/phonetic-alphabet-converter.js'
+    : 'https://unpkg.com/phonetic-alphabet-converter@latest/umd/phonetic-alphabet-converter.js';
+script.addEventListener('load', main);
+document.head.appendChild(script);
+
+/**
+ * Main.
+ */
+function main() {
+  var converter = window.PhoneticAlphabetConverter.default;
+  var MDCChipSet = mdc.chips.MDCChipSet;
+  var chipSet = document.querySelector('.mdc-chip-set');
+
+  // Instantiate single textfield component rendered in the document
+  mdc.textField.MDCTextField.attachTo(
+    document.querySelector('.mdc-text-field')
+  );
+
+  /**
+   * @param {string} text
+   * @return {string}
+   */
+  function generateChipMarkup(text) {
+    var html = '<span class="mdc-chip__text">' + text + '</span>';
+    html += '<div class="mdc-chip__ripple"></div>';
+    html = '<div class="mdc-chip">' + html + '</div>';
+    return html;
+  }
+
+  // Convert input to NATO phonetic alphabet text
+  var input = document.querySelector('input');
+  input.focus();
+  input.addEventListener('input', function(event) {
+    var text = event.target.value;
+    var words = converter(text);
+    var chips = words.map(function(word) {
+      return word ? generateChipMarkup(word) : null;
+    });
+    chipSet.innerHTML = chips.join('');
+    new MDCChipSet(chipSet);
+  });
+}
